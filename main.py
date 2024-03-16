3bb9d965efb67715b2b09f6fb3c7918e627e8e82
Milton <sandovalmonasteriom@gmail.com>
2024-03-16 15:42:53 -0400

    get users terminado

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from pydantic import BaseModel
 app = FastAPI()
 
 class usuarios(BaseModel):
+    id : int
     name : str
     surname : str
     url : str
     age : int
 
-usuarios_list = [usuarios(name = "Milton", surname = "Sandoval", url = "https://Milton.dev.com", age = 19),
-                 usuarios(name = "Pardo", surname = "Osuno", url = "https://pardososuno.com", age = 20),
-                 usuarios(name = "Rodolfo", surname = "Maloso", url = "https://rodolfomaloso.com.bo", age = 35)]
+usuarios_list = [usuarios(id = 1, name = "Milton", surname = "Sandoval", url = "https://Milton.dev.com", age = 19),
+                 usuarios(id = 2, name = "Pardo", surname = "Osuno", url = "https://pardososuno.com", age = 20),
+                 usuarios(id = 3, name = "Rodolfo", surname = "Maloso", url = "https://rodolfomaloso.com.bo", age = 35)]
 
 
 @app.get("/users")
@@ -22,3 +23,18 @@ async def users():
 async def userlist():
     return usuarios_list
 
+@app.get("/users/{ids}")
+async def user(ids:int):
+    return search_id(ids)
+
+@app.get("/users/")
+async def user(ids:int):
+    return search_id(ids)
+
+
+def search_id(ids:int):
+    for usuarios in usuarios_list:
+        if ids == usuarios.id:
+            return usuarios
+    else:
+        return {"error" : "No se a encontrado el usuario"}
